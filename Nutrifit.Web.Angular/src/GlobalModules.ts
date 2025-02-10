2d641425a9eb5fe94da03d3af22295b250ef19f4
Gabriel Weibel <gabriel.weibel@hotmail.de>
2025-02-10 18:21:59 +0100

    updated depricated http client registration

--- Nutrifit.Web.Angular/src/GlobalModules.ts
+++ Nutrifit.Web.Angular/src/GlobalModules.ts
@@ -1,5 +1,4 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
-export const globalModules = [CommonModule, RouterModule, HttpClientModule];
+export const globalModules = [CommonModule, RouterModule];
