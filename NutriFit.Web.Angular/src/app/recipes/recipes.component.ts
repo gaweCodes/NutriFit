9becf5777bfb95187f971a173b47fd3f18e98820
Gabriel Weibel <gabriel.weibel@hotmail.de>
2025-02-11 12:09:22 +0100

    create recipe form

--- NutriFit.Web.Angular/src/app/recipes/recipes.component.ts
+++ NutriFit.Web.Angular/src/app/recipes/recipes.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { globalModules } from '../../GlobalModules';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'nutrifit-recipes',
-  imports: [],
   templateUrl: './recipes.component.html',
-  styleUrl: './recipes.component.scss'
+  styleUrl: './recipes.component.scss',
+  imports: [globalModules],
 })
 export class RecipesComponent {
-
+  private readonly formBuilder = inject(NonNullableFormBuilder);
+  public readonly recipeForm = this.formBuilder.group({
+    name: ['', { validators: [Validators.required, Validators.minLength(1)] }],
+  });
 }
--- Nutrifit.Web.Angular/src/GlobalModules.ts
+++ Nutrifit.Web.Angular/src/GlobalModules.ts
@@ -1,4 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-export const globalModules = [CommonModule, RouterModule];
+export const globalModules = [
+  CommonModule,
+  RouterModule,
+  ReactiveFormsModule,
+  ReactiveFormsModule,
+];
