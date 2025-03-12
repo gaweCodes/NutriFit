179173534abfaa1045bbec616b4f07bb9778b0a7
Gabriel Weibel <gabriel.weibel@hotmail.de>
2025-03-13 00:10:34 +0100

    fixed toaster handling

--- NutriFit.Web.Angular/src/shared/interceptors/api-error-interceptor.ts
+++ NutriFit.Web.Angular/src/shared/interceptors/api-error-interceptor.ts
@@ -1,12 +1,13 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
-import { catchError, throwError } from 'rxjs';
+import { catchError, tap, throwError } from 'rxjs';
 import { ToasterService } from '../services/toaster-service';
 
 export const apiErrorInterceptor: HttpInterceptorFn = (req, next) => {
   const toasterService = inject(ToasterService);
   return next(req).pipe(
+    tap(() => toasterService.clear()),
     catchError((error) => {
       if (error instanceof HttpErrorResponse) {
         let errorMessage = 'Ein unerwarteter Fehler ist aufgetreten.';
--- NutriFit.Web.Angular/src/shared/services/toaster-service.ts
+++ NutriFit.Web.Angular/src/shared/services/toaster-service.ts
@@ -9,7 +9,15 @@ import { provideToastr, ToastrService } from 'ngx-toastr';
 @Injectable({ providedIn: 'root' })
 export class ToasterService {
   private toastrService = inject(ToastrService);
+  public clear(): void {
+    this.toastrService.clear();
+  }
+
   public showError(message: string, title: string): void {
+    this.toastrService
+      .clear
+      // this.toastrService.findDuplicate(title, message, false, false).toastId
+      ();
     this.toastrService.error(message, title, {
       timeOut: 0,
       extendedTimeOut: 0,
